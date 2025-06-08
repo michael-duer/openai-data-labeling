a45e8b7d91f3a6e37202a120faccf15cf0a260cc
Michael Dür <62062370+michael-duer@users.noreply.github.com>
2025-06-08 18:48:37 +0200

    Add directional relation columns to samples
    - Update R script to convert sample files from a single "relation" column to two columns: "rel_head_tail" and "rel_tail_head", allowing explicit directional labeling.
    - Migrate all sample files in the samples/ folder to the new structure.
    - Keep previous version of the samples in folder archived_samples/.

--- scripts/make_relationships_bidirectional.R
+++ scripts/make_relationships_bidirectional.R
@@ -1,27 +1,59 @@
 library(dplyr)
 library(stringr)
 
-# This script adjusts the sample data by establishing bidirectional relationships. After applying the script to a file, the "relation" column will be replaced by two columns: "rel_head_tail" and "rel_tail_head". The labels will also change from positive1, positive2, neutral1, neutral2, etc. to positive/neutral/negative/none.
+# This script adjusts the relationships in all files inside the "../data/samples/" 
+# folder to be bidirectional. The "relation" column will be replaced by the two 
+# columns: "rel_head_tail" and "rel_tail_head". The labels will also change from 
+# positive1,positive2,neutral1,neutral2, etc. to positive/neutral/negative/none.
 
 setwd("~/Documents/GitHub/openai-data-labeling/scripts")
 
-# Load file to apply changes to
-data <- read.csv("../data/random_sample_small_1.csv",sep = ",")
-
-# Adjust columns
-data_adjusted <- data %>%
-    rename(relation = "rel_head_tail") %>%
+# Helper Function to adjust columns and relationships
+adjust_relationships <- function(data){
+  data %>%
+    rename(rel_head_tail = relation) %>%
     mutate(
-        # Add relationship in other direction by either copying the relationship (if mutual) or add "none" (if one-sided)
-        "rel_tail_head" = "rel_head_tail" %>% case_when(
-            # If last char = 1 (one-sided) -> set relation to none
-            str_sub("rel_head_tail",-1) == "1" ~ "none"
-            # If last char = 2 (mutual) -> keep relation and remove number
-            str_sub("rel_head_tail",-1) == "2" ~ sub("2","","rel_head_tail")
-            # If last char of string is neither (probably none) -> keep relation
-            TRUE ~ "rel_head_tail")
-        # Remove number from relationship type
-        "rel_head_tail" = gsub("[1-2]","","rel_head_tail"))
+      # Add relationship in other direction by either copying the relationship 
+      # (if mutual) or add "none" (if one-sided)
+      rel_tail_head = case_when(
+        # If one-sided -> set relation to none
+        str_sub(rel_head_tail,-1) == "1" ~ "none",
+        # If mutual -> keep relation and remove number
+        str_sub(rel_head_tail,-1) == "2" ~ sub("2","",rel_head_tail),
+        # If last char of string is neither (probably none) -> keep relation
+        TRUE ~ rel_head_tail
+      ),
+      # Remove number from relationship type
+      rel_head_tail = gsub("[1-2]","",rel_head_tail)
+    )
+}
 
+# Read all files in "samples" folder
+files <- list.files(path = "../data/samples", pattern = "*.csv", full.names = TRUE)
 
-# Export file with adjusted name
+# Apply transformation to all files
+for (file in files) {
+  # Exclude already transformed files
+  if (file %in% c("../data/samples/random_sample_small_1 bidirectional.csv",
+                  "../data/samples/random_sample_small_5_bidirectional.csv")) next
+  
+  tryCatch(
+    {
+      data <- read.csv(file,sep = ",")
+      transformed_data <- adjust_relationships(data)
+      # Save with same name to output directory
+      filename <- basename(file)
+      write.csv(transformed_data, file=paste0("../data/samples/new/",filename), row.names = FALSE)
+    },
+    # Print error message
+    error=function(e) {
+      message(paste0("An Error occurred while processing ",file))
+      print(e)
+    },
+    # Print warning message
+    warning=function(w) {
+      message(paste0("A Warning occurred while processing ",file))
+      print(w)
+    }
+  )
+}
